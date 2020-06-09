Added l2 normalization to the SBL algorithm and some bug fixed in the state-space model

examples/Rossler_system_GP.py
@@ -12,22 +12,22 @@ from ode_composer.signal_preprocessor import (
 )
 
 
-# define Lotka-Volerra model
-states = {"x": "1.5*sin(y)", "y": "0.1*c", "z": "b + 5*sin(x)"}
-parameters = {"a": 0.2, "b": 0.5, "c": 5.7}
+# define the model
+states = {"x": "-y-z", "y": "x+a*y", "z": "b+x*z-z*c"}
+parameters = {"a": 0.2, "b": 0.2, "c": 5.7}
 ss = StateSpaceModel.from_string(states=states, parameters=parameters)
 print("Original Model:")
 print(ss)
 
-t_span = [0, 20]
-y0 = {"x": 0.01, "y": 0.1, "z": 0.000001}
-data_points = 250
+t_span = [0, 30]
+y0 = {"x": 1.0, "y": 10.0, "z": 10.0}
+data_points = 1000
 gp_interpolation_factor = 1
 # simulate model
 gm = MeasurementsGenerator(
     ss=ss, time_span=t_span, initial_values=y0, data_points=data_points
 )
-t, y = gm.get_measurements(SNR_db=25)
+t, y = gm.get_measurements(SNR_db=50)
 
 # Fit a GP, find the derivative and std of each
 gproc = GPSignalPreprocessor(
@@ -44,7 +44,7 @@ dxdt_std = gproc.A_std
 gproc_2 = GPSignalPreprocessor(
     t=t,
     y=y[1, :],
-    selected_kernel="Matern",
+    selected_kernel="Matern*ExpSineSquared",
     interpolation_factor=gp_interpolation_factor,
 )
 y_samples, _ = gproc_2.interpolate()
@@ -52,10 +52,11 @@ gproc_2.calculate_time_derivative()
 dydt = gproc_2.dydt
 dydt_std = gproc_2.A_std
 
-gproc_3 = GPSignalPreprocessor(t=t,
-                               y=y[2, :],
-                               selected_kernel="RatQuad*ExpSineSquared",
-                               interpolation_factor=gp_interpolation_factor,
+gproc_3 = GPSignalPreprocessor(
+    t=t,
+    y=y[2, :],
+    selected_kernel="Matern",
+    interpolation_factor=gp_interpolation_factor,
 )
 z_samples, _ = gproc_3.interpolate()
 gproc_3.calculate_time_derivative()
@@ -82,7 +83,7 @@ plt.figure(0)
 plt.plot(y[0,:])
 plt.plot(y[1,:])
 plt.plot(y[2,:])
-plt.title("Right Hand Side evaluation (prior knowledge of signal)")
+plt.title("Observable states")
 plt.legend(['x', 'y', 'z'])
 plt.show()
 rhs_preprop.calculate_time_derivative()
@@ -93,7 +94,7 @@ dx3 = dydt_rhs[2, :]
 
 plt.figure(1)
 plt.subplot(311)
-plt.scatter(t, dx1, label="RHS diff")
+plt.plot(t, dx1, label="RHS diff")
 plt.plot(t, dx_spline, c='r', label="Spline diff")
 plt.plot(t_gp, dxdt, c='orange', label="GP diff")
 plt.fill_between(t_gp, dxdt - 2*dxdt_std, dxdt + 2*dxdt_std, alpha=0.2, color='k')
@@ -101,7 +102,7 @@ plt.ylabel("derivative of x")
 plt.xlabel("time")
 plt.legend(loc="best")
 plt.subplot(312)
-plt.scatter(t, dx2, label="RHS diff")
+plt.plot(t, dx2, label="RHS diff")
 plt.plot(t, dy_spline, c='r', label="Spline diff")
 plt.plot(t_gp, dydt, c='orange', label="GP diff")
 plt.fill_between(t_gp, dydt - 2*dydt_std, dydt + 2*dydt_std, alpha=0.2, color='k')
@@ -109,7 +110,7 @@ plt.ylabel("derivative of the y")
 plt.xlabel("time")
 plt.legend(loc="best")
 plt.subplot(313)
-plt.scatter(t, dx3, label="RHS diff")
+plt.plot(t, dx3, label="RHS diff")
 plt.plot(t, dz_spline, c='r', label="spline diff")
 plt.plot(t_gp, dzdt, c='orange', label="GP diff")
 plt.fill_between(t_gp, dzdt - 2*dzdt_std, dzdt + 2*dzdt_std, alpha=0.2, color='k')
@@ -119,34 +120,33 @@ plt.legend(loc="best")
 plt.show()
 
 # step 1 define a dictionary
-d_f = ["1", "x", "y", "z", "z*x", "sin(x)", "sin(y)", "x/z", "y/z", "z/x", "y/x", "x**2", "y**2", "z**2",
-       "1/(x+sin(x))", "1/tan(y)", "sin(2*x)"]
+d_f = ["1", "x", "y", "z", "z*x"]
 dict_builder = DictionaryBuilder(dict_fcns=d_f)
 # associate variables with data
-data = {"x": x_samples.T, "y": y_samples.T, "z": z_samples.T}
+data = {"x": x_spline.T, "y": y_spline.T, "z": z_spline.T}
 A = dict_builder.evaluate_dict(input_data=data)
 
 # step 2 define an SBL problem
 # with the Lin reg model and solve it
-lambda_param = 5.0
+lambda_param = 10.0
 lambda_param_1 = (np.mean(dxdt_std)) + 0.1
 print(lambda_param_1)
 sbl_x1 = SBL(
-    dict_mtx=A, data_vec=dxdt, lambda_param=lambda_param_1, dict_fcns=d_f
+    dict_mtx=A, data_vec=dx_spline, lambda_param=lambda_param_1, dict_fcns=d_f
 )
 sbl_x1.compute_model_structure()
 
 lambda_param_2 = (np.mean(dydt_std)) + 0.1
 print(lambda_param_2)
 sbl_x2 = SBL(
-    dict_mtx=A, data_vec=dydt, lambda_param=lambda_param_2, dict_fcns=d_f
+    dict_mtx=A, data_vec=dy_spline, lambda_param=lambda_param_2, dict_fcns=d_f
 )
 sbl_x2.compute_model_structure()
 
 lambda_param_3 = (np.mean(dzdt_std)) + 0.1
 print(lambda_param_3)
 sbl_x3 = SBL(
-    dict_mtx=A, data_vec=dzdt, lambda_param=lambda_param_3, dict_fcns=d_f
+    dict_mtx=A, data_vec=dz_spline, lambda_param=lambda_param_3, dict_fcns=d_f
 )
 sbl_x3.compute_model_structure()
 
@@ -166,7 +166,7 @@ ode_model = StateSpaceModel(
 print("Estimated ODE model:")
 print(ode_model)
 states = ["x", "y", "z"]
-y0 = [0.01, 0.1, 0.00001]
+y0 = [1.0, 10.0, 10.0]
 sol_ode = solve_ivp(fun=ode_model.get_rhs, t_span=t_span, t_eval=t_gp, y0=y0, args=(states,))
 
 plt.figure(2)

ode_composer/sbl.py
@@ -15,7 +15,11 @@ class SBL(object):
         lambda_param: float,
         dict_fcns: List[str] = None,
     ):
-        self.linear_model = LinearModel(dict_mtx, data_vec=data_vec)
+        #L2-normalization D.Wipf et. al - Iterative reweighted l1 and l2 methods for finding sparse solutions
+        self.norm_vect = np.linalg.norm(dict_mtx, axis=0)
+        self.norm_dict = dict_mtx/self.norm_vect
+
+        self.linear_model = LinearModel(self.norm_dict, data_vec=data_vec)
         self.z = np.ones((self.linear_model.parameter_num, 1))
         self.w_estimates: List[float] = list()
         self.z_estimates: List[float] = list()
@@ -92,7 +96,8 @@ class SBL(object):
 
     def compute_model_structure(self):
         # TODO transform this into a generator
-        for _ in range(100):
+        # TODO: stop loop upon convergence
+        for _ in range(500):
             self.estimate_model_parameters()
             self.update_z()
 
@@ -103,7 +108,8 @@ class SBL(object):
         if len(self.w_estimates) == 0:
             w_est = np.zeros((self.linear_model.parameter_num, 1))
         else:
-            w_est = self.w_estimates[-1]
+            # Recover back parameters after dict normalization
+            w_est = self.w_estimates[-1]/self.norm_vect
             if zero_th is not None:
                 zero_idx = [
                     idx for idx, w in enumerate(w_est) if abs(w) <= zero_th