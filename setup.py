c1063ee0f3abad1f63c70104567f512793068896
zoltuz <zoltuz@gmail.com>
2020-10-12 19:57:57 +0100

    add pandas as a dependency
    Pandas pakcages is needed for the Database object to work properly.

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
         "matplotlib",
         "cvxpy",
         "sklearn",
+        "pandas",
     ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest", "pytest-cov", "cvxpy"],
